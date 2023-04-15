2884f3892dba9f59d734057c8dade5405dfc6a0b
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-04-15 18:23:07 +0300

    fixed for ubuntu

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,9 +6,9 @@ export HOST_PORT=5020
 export CONTAINER_PORT=5020
 
 #!/bin/bash
-sudo yum -y update
-sudo yum -y install docker
-sudo service docker start
+sudo apt-get update
+sudo apt-get install -y docker.io
+sudo systemctl start docker
 sudo docker login -u AWS -p $(aws ecr get-login-password --region $AWS_REGION) $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 sudo docker pull $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
 sudo docker run -d -p $HOST_PORT:$CONTAINER_PORT $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$IMAGE_NAME:$IMAGE_TAG
