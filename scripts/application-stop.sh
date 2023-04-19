5b7326bd5419e68642af80f160a8f2366b728aee
Vladimir Golovko <48762632+MrLait@users.noreply.github.com>
2023-04-19 17:17:10 +0300

    in app-stop added delete images

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# pune all images
+# prune all images
 sudo docker container stop $(sudo docker container ls -aq)
 sudo docker container prune --force
+
+# Get IDs of all images with name "my-app" and delete them
+IMAGE_IDS=$(sudo docker images -q my-app)
+
+# Loop through all image IDs and remove each one
+for ID in $IMAGE_IDS
+do
+  sudo docker rmi $ID
+done
